039ddcee3091214e9243f6906ee10edcd7ec0b2c
dustin-chan-89 <busterdust@gmail.com>
2015-12-17 18:00:49 -0800

    removed sqlite3 gem

--- frontend/components/SignUp.jsx
+++ frontend/components/SignUp.jsx
@@ -3,8 +3,21 @@ var React = require('react');
 var SignUp = React.createClass({
 	render: function () {
 		return (
-			<div className="sign-up-box">
-				
+			<div id="sign-up-background" className="ui one column stackable center aligned page grid">
+				<div className="column seven wide">
+					<div className="ui form">
+					  <div className="fields">
+					    <div className="field">
+					      <label>Username</label>
+					      <input type="text" placeholder="Username"/>
+					    </div>
+					    <div className="field">
+					      <label>Password</label>
+					      <input type="password" placeholder="Password"/>
+					    </div>
+					  </div>
+					</div>
+				</div>
 			</div>
 		);
 	}
