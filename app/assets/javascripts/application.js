3e9699caa4d0203edb832789f4d678f220b22330
dustin-chan-89 <busterdust@gmail.com>
2015-12-17 18:06:46 -0800

    heroku stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require bundle
+//= require jquery-ui/datepicker
+//= require jquery-ui/dialog
+//= require jquery-ui/sortable
+//= require jquery-ui/widget
+//= require jquery_ujs
+//= require_self
