48dd01920d7afe253c108b5501acb375c5d0a019
dustin-chan-89 <busterdust@gmail.com>
2016-01-01 18:13:47 -0800

    fixed syntax error in movielists_controller.rb

--- app/controllers/api/movielists_controller.rb
+++ app/controllers/api/movielists_controller.rb
@@ -1,5 +1,5 @@
 class MovielistsController < ApplicationController
-	before_action :require_signed_in!, except [:index]
+	before_action :require_signed_in!, except: [:index]
 	before_action :require_user_owns_post!, only: [:edit, :update]
 
 	def new
