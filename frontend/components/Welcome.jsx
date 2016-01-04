8819cffee7a5c1efd26bf61df436ac7434ec6386
dustin-chan-89 <busterdust@gmail.com>
2016-01-04 13:29:05 -0800

    fixed scaling problems

--- frontend/components/Welcome.jsx
+++ frontend/components/Welcome.jsx
@@ -7,8 +7,10 @@ var Welcome = React.createClass({
 					<img id="welcome-background-image" src='./welcome-backdrop.jpg'/>
 					<h1 className="welcome header">Find something to watch.</h1>
 					<h2 className="welcome subheader">Quickly browse a database of over 250,000 movies.</h2>
-					<a href="#/browse" id="browse-movies-button" className="ui inverted blue basic button">BROWSE MOVIES</a>
-					<a href="#/lists" id="browse-lists-button" className="ui inverted blue basic button">BROWSE LISTS</a>
+					<div className="welcome buttons container">
+						<a href="#/browse" id="browse-movies-button" className="ui inverted blue basic button">BROWSE MOVIES</a>
+						<a href="#/lists" id="browse-lists-button" className="ui inverted blue basic button">BROWSE LISTS</a>
+					</div>
 			</div>
 		)
 
